e24804637c4c31fa9c6424cd11e0bb2413a9d924
MykolaLebid <mykola.lebid@gmail.com>
2019-12-27 00:43:29 +0200

    clang format

--- src/matrix.h
+++ src/matrix.h
@@ -1,209 +1,199 @@
+
 #ifndef Matrix_h
 #define Matrix_h
 
-//#include <iostream>
-#include <vector>
-#include <memory>
-#include <string>
 #include <algorithm>
 #include <functional>
-//#include <stdexcept> 
+#include <memory>
+#include <string>
+#include <vector>
 
-namespace mtx{
-//ADDITIONS 
-//we can put parameters rowN and colN as additional template types 
+namespace mtx {
+// ADDITIONS
+// we can put parameters rowN and colN as additional template types
 //"template <type NumType, rowN, colN>"
-//to do the job at the compile time with fixed size matix, but we lose runtime flexibility.
-//So, class design depences on an exersice.
-template<typename NumType> 
-class Matrix
-{
-	// we want to work with arithmetic types
-	static_assert(std::is_arithmetic<NumType>::value,"");
-	public:
-//Special member functions	
-	//constructor	
-	Matrix():rowN_{0}, colN_{0}{};
-	Matrix(const size_t _rowN, const size_t _colN);
-	Matrix(const std::vector<NumType>& vec):
-		rowN_{1}, colN_{vec.size()}, m{vec}{};// for vectors
-	//copying 
-	Matrix(const Matrix&) = default;
-	Matrix& operator=(const Matrix&) = delete;
-	//moving
-	Matrix(Matrix&&) = default;
-	Matrix& operator=(Matrix &&) = default;
-        //destuctor	
-	//virtual ~Matrix(){};
-//Mathematical operations with matrices
-	Matrix operator+(const Matrix& rhs);
-	void operator+=(const Matrix& rhs);
-	//SCALAR multiplication
-	Matrix& operator*=(const NumType& rhs);
-	// lhs miltiplication is added too
-	Matrix  operator*(const NumType& rhs); 	
-	//MATRIX multiplication
-	Matrix operator*(const Matrix& rhs);
-	Matrix operator&(const Matrix& rhs);//optimized multiplication
-	Matrix getTranspose() const;
-//Set and Get
-//TODO create one from the following 2 functions
-	NumType& operator() (size_t row, size_t col); // Set        
-        NumType  operator() (size_t row, size_t col) const; // Get 
-	private:		
-//Main data structures
-	size_t rowN_;
-	size_t colN_;
-	size_t m_size;	
-	using m_type = std::vector<NumType>;
-	m_type m; 
-//Check exeption functions
-	void check_range(const size_t rowIndex,
-		         const size_t colIndex);
-	void is_dim_equal_with(const Matrix& rhs) const;
-	void is_multi_possible(const Matrix& rhs) const;
-};// class Matrix{
+// to do the job at the compile time with fixed size matix, but we lose runtime
+// flexibility. So, class design depences on an exersice.
+template <typename NumType> class Matrix {
+  // we want to work with arithmetic types
+  static_assert(std::is_arithmetic<NumType>::value, "");
+
+public:
+  // Special member functions
+  // Constructors
+  Matrix() : rowN_{0}, colN_{0} {};
+  Matrix(const size_t _rowN, const size_t _colN);
+  Matrix(const std::vector<NumType> &vec)
+      : rowN_{1}, colN_{vec.size()}, m{vec} {}; // for vectors
+  // Copying
+  Matrix(const Matrix &) = default;
+  Matrix &operator=(const Matrix &) = delete;
+  // Moving
+  Matrix(Matrix &&) = default;
+  Matrix &operator=(Matrix &&) = default;
+  // Mathematical operations with matrices
+  Matrix operator+(const Matrix &rhs);
+  void operator+=(const Matrix &rhs);
+  // SCALAR multiplication
+  Matrix &operator*=(const NumType &rhs);
+  // lhs miltiplication is added too
+  Matrix operator*(const NumType &rhs);
+  // MATRIX multiplication
+  Matrix operator*(const Matrix &rhs);
+  Matrix operator&(const Matrix &rhs); // optimized multiplication
+  Matrix getTranspose() const;
+  // Setter and Getter
+  // TODO create one from the following 2 functions
+  NumType &operator()(size_t row, size_t col);      // Set
+  NumType operator()(size_t row, size_t col) const; // Get
+private:
+  // Main data structures
+  size_t rowN_;
+  size_t colN_;
+  size_t m_size;
+  using m_type = std::vector<NumType>;
+  m_type m;
+  // Check exeption functions
+  void check_range(const size_t rowIndex, const size_t colIndex);
+  void is_dim_equal_with(const Matrix &rhs) const;
+  void is_multi_possible(const Matrix &rhs) const;
+}; // class Matrix{
 
 // Exception handling
-class MatrixException: public std::exception
-{
+class MatrixException : public std::exception {
 private:
-	std::string m_error;
+  std::string m_error;
+
 public:
-	MatrixException(std::string error) : m_error(error){};
-	const char* what() const noexcept { return m_error.c_str(); }
-};// class MatrixException
+  MatrixException(std::string error) : m_error(error){};
+  const char *what() const noexcept { return m_error.c_str(); }
+}; // class MatrixException
 
 ////////////////////
-//Definition
+// Definitions
 ////////////////////
 
-
-//constructor
-template<typename NumType>
-inline Matrix<NumType>::Matrix(const size_t _rowN,const size_t _colN):
-      	rowN_{_rowN}, colN_{_colN}, m_size{_rowN*_colN}{
-	try { 		
-		m.resize(m_size);	
-	// vector throws a length_error if resized above max_size
-	} catch (const std::length_error& le) {
-  		throw MatrixException("out of memory");
-	}
-	if (m_size == 0) throw MatrixException("degenerate matrix");
+// Constructors
+template <typename NumType>
+inline Matrix<NumType>::Matrix(const size_t _rowN, const size_t _colN)
+    : rowN_{_rowN}, colN_{_colN}, m_size{_rowN * _colN} {
+  try {
+    m.resize(m_size);
+    // vector throws a length_error if resized above max_size
+  } catch (const std::length_error &le) {
+    throw MatrixException("out of memory");
+  }
+  if (m_size == 0)
+    throw MatrixException("degenerate matrix");
 }
-
-
-template<typename NumType>
-inline NumType& Matrix<NumType>::operator()(size_t row, size_t col){
-	try {
-		return m.at(row*colN_ + col);
-        } catch(const std::out_of_range& oor){
-		throw MatrixException("matrix index out of range");
-	}
+// Setter
+template <typename NumType>
+inline NumType &Matrix<NumType>::operator()(size_t row, size_t col) {
+  try {
+    return m.at(row * colN_ + col);
+  } catch (const std::out_of_range &oor) {
+    throw MatrixException("matrix index out of range");
+  }
 }
-
-template<typename NumType>
+// Getter
+template <typename NumType>
 inline NumType Matrix<NumType>::operator()(size_t row, size_t col) const {
-	try {
-		return m.at(row*colN_ + col);
-        } catch(const std::out_of_range& oor){
-		throw MatrixException("matrix index out of range");
-	};
+  try {
+    return m.at(row * colN_ + col);
+  } catch (const std::out_of_range &oor) {
+    throw MatrixException("matrix index out of range");
+  };
+}
+// Setter
+template <typename NumType>
+inline void Matrix<NumType>::is_dim_equal_with(const Matrix &rhs) const {
+  if ((rowN_ != rhs.rowN_) || (colN_ != rhs.colN_))
+    throw MatrixException("different size matrices");
+}
+// Exception functions
+template <typename NumType>
+inline void Matrix<NumType>::is_multi_possible(const Matrix &rhs) const {
+  if (colN_ != rhs.rowN_)
+    throw MatrixException("matrix miltiplication is mot pssible");
+}
+// Matix addition
+template <typename NumType>
+inline void Matrix<NumType>::operator+=(const Matrix &rhs) {
+  // TODO parallelisation with respect to size of a vector C++17
+  is_dim_equal_with(rhs);
+  std::transform(m.begin(), m.end(), rhs.m.begin(), m.begin(),
+                 std::plus<NumType>());
 }
 
-template<typename NumType>
-inline void Matrix<NumType>::is_dim_equal_with(const Matrix& rhs) const{
-	if ((rowN_ != rhs.rowN_) || (colN_ != rhs.colN_))
-		throw MatrixException("different size matrices");
-}
-template<typename NumType>
-inline void Matrix<NumType>::is_multi_possible(const Matrix& rhs) const{
-	if (colN_!=rhs.rowN_)
-		throw MatrixException("matrix miltiplication is mot pssible");
-}
-template<typename NumType>
-inline void Matrix<NumType>::operator+=(const Matrix& rhs){
-	//TODO parallelisation with respect to size of a vector C++17
-	is_dim_equal_with(rhs);
-	std::transform (m.begin(), m.end(), 
-			rhs.m.begin(), m.begin(), 
-			std::plus<NumType>());
+template <typename NumType>
+inline Matrix<NumType> Matrix<NumType>::operator+(const Matrix &rhs) {
+  is_dim_equal_with(rhs);
+  Matrix<NumType> m_local = (*this);
+  m_local += rhs;
+  return m_local; // named RWO is expacted
 }
 
-template<typename NumType>
-inline Matrix<NumType> Matrix<NumType>::operator+(const Matrix& rhs){ 
-	is_dim_equal_with(rhs);
-	Matrix<NumType> m_local = (*this);
-	m_local+=rhs;
-	return m_local; //named RWO is expacted
+// SCALAR multiplication 
+template <typename NumType>
+inline Matrix<NumType> &Matrix<NumType>::operator*=(const NumType &rhs) {
+  // TODO parallelisation with respect to size of a vector C++17
+  transform(m.begin(), m.end(), m.begin(), [&rhs](auto &c) { return c * rhs; });
+  return *this;
 }
 
-template<typename NumType>
-inline Matrix<NumType>& Matrix<NumType>::operator*=(const NumType& rhs){
-	//TODO parallelisation with respect to size of a vector C++17
-	transform(m.begin(), m.end(), 
-		  m.begin(), 
-		  [&rhs](auto& c){return c * rhs;});	
-	return *this;
+template <typename NumType>
+inline Matrix<NumType> Matrix<NumType>::operator*(const NumType &rhs) {
+  Matrix<NumType> m_local = (*this);
+  m_local *= rhs;
+  return m_local; // named RWO is expacted
 }
 
-template<typename NumType>
-inline Matrix<NumType> Matrix<NumType>::operator*(const NumType& rhs){
-	Matrix<NumType> m_local = (*this);
-	m_local*=rhs;
-	return m_local; //named RWO is expacted
+template <typename NumType>
+inline Matrix<NumType> operator*(const NumType &lhs,
+                                 const Matrix<NumType> &rhs) {
+  Matrix<NumType> m_local = rhs;
+  m_local *= lhs;
+  return m_local; // named RWO is expacted
 }
 
-template<typename NumType>
-inline Matrix<NumType> operator*(const NumType& lhs, const Matrix<NumType>& rhs){
-	Matrix<NumType> m_local = rhs;
-	m_local*=lhs;
-	return m_local; //named RWO is expacted
-}
-
-template<typename NumType>
-inline Matrix<NumType> Matrix<NumType>::getTranspose() const{	
-	Matrix<NumType> m_local(colN_, rowN_);
-	for(unsigned int i=0; i<colN_; i++)
-		for(unsigned int j=0; j<rowN_; j++)
-			m_local.m[i * rowN_ + j] = m[j * colN_ + i];
-	return m_local;
+template <typename NumType>
+inline Matrix<NumType> Matrix<NumType>::getTranspose() const {
+  Matrix<NumType> m_local(colN_, rowN_);
+  for (unsigned int i = 0; i < colN_; i++)
+    for (unsigned int j = 0; j < rowN_; j++)
+      m_local.m[i * rowN_ + j] = m[j * colN_ + i];
+  return m_local;
+};
+// Naive multiplication
+template <typename NumType>
+inline Matrix<NumType> Matrix<NumType>::operator*(const Matrix &rhs) {
+  is_multi_possible(rhs);
+  Matrix<NumType> result_m(rowN_, rhs.colN_);
+  for (size_t i = 0; i < rowN_; i++)
+    for (size_t j = 0; j < rhs.colN_; j++) {
+      size_t index = i * rhs.colN_ + j;
+      result_m.m[index] = 0;
+      for (unsigned int k = 0; k < colN_; k++)
+        result_m.m[index] += m[i * colN_ + k] * rhs.m[k * rhs.colN_ + j];
+    };
+  return result_m;
+};
+// Multiplication with transposition
+template <typename NumType>
+inline Matrix<NumType> Matrix<NumType>::operator&(const Matrix &rhs) {
+  is_multi_possible(rhs);
+  Matrix<NumType> result_m(rowN_, rhs.colN_);
+  Matrix<NumType> tr(rhs.getTranspose());
+  for (size_t i = 0; i < rowN_; i++)
+    for (size_t j = 0; j < tr.rowN_; j++) {
+      size_t index = i * tr.rowN_ + j;
+      result_m.m[index] = 0;
+      for (unsigned int k = 0; k < colN_; k++)
+        result_m.m[index] += m[i * colN_ + k] * tr.m[j * tr.colN_ + k];
+    };
+  return result_m;
 };
 
-//TODO Compare with Strassen’s Matrix Multiplication 
-template<typename NumType>
-inline Matrix<NumType> Matrix<NumType>::operator*(const Matrix& rhs){
-	is_multi_possible(rhs);
-	Matrix<NumType> result_m(rowN_,rhs.colN_);
-	for(size_t i=0; i < rowN_; i++)
-		for(size_t j=0; j < rhs.colN_; j++){
-			size_t index = i*rhs.colN_ + j; 
-			result_m.m[index] = 0;
-			for(unsigned int k = 0; k <colN_; k++)
-				result_m.m[index]+= 
-					m[i*colN_ + k] * 
-					rhs.m[k*rhs.colN_ + j];
-		};
-	return result_m;	
-};
-
-
-template<typename NumType>
-inline Matrix<NumType> Matrix<NumType>::operator&(const Matrix& rhs){
-	is_multi_possible(rhs);
-	Matrix<NumType> result_m(rowN_,rhs.colN_);
-       	Matrix<NumType> tr(rhs.getTranspose());	
-	for(size_t i=0; i < rowN_; i++)
-		for(size_t j=0; j < tr.rowN_; j++){
-			size_t index = i*tr.rowN_ + j; 
-			result_m.m[index] = 0;
-			for(unsigned int k = 0; k <colN_; k++) 
-				result_m.m[index]+= 
-					m[i*colN_ + k] * 
-					tr.m[j*tr.colN_ + k];
-		};
-	return result_m;	
-};
-
-};//namespace mtx{ 
+}; // namespace mtx
 #endif
+
--- tests/MatrixTest.cpp
+++ tests/MatrixTest.cpp
@@ -1,294 +1,304 @@
-#include<gmock/gmock.h>
-// src file for matrix
-#include<matrix.h> 
 
-#include <iostream>       
-//#include <stdexcept>      
+#include <gmock/gmock.h>
+// src file for matrix
+#include <matrix.h>
+
 using namespace mtx;
 
-TEST(TestMatrix, CheckOutOfMemoryExeption){
-bool is_thrown = false;
+TEST(TestMatrix, CheckOutOfMemoryExeption) {
+  bool is_thrown = false;
 
-try{
-	std::vector<float> v_f;
-	Matrix<float> mt(v_f.max_size(),2);}
-catch(const MatrixException& me ){
-	is_thrown = true;
-	ASSERT_STREQ( "out of memory", me.what());}
+  try {
+    std::vector<float> v_f;
+    Matrix<float> mt(v_f.max_size(), 2);
+  } catch (const MatrixException &me) {
+    is_thrown = true;
+    ASSERT_STREQ("out of memory", me.what());
+  }
 
-ASSERT_THAT(is_thrown, testing::Eq(true));
+  ASSERT_THAT(is_thrown, testing::Eq(true));
 };
 
-TEST(TestMatrix, CheckDegenerateMatrixExeption){
-bool is_thrown = false;
+TEST(TestMatrix, CheckDegenerateMatrixExeption) {
+  bool is_thrown = false;
 
-try{	
-	Matrix<double> mt(12,0);} 
-catch (const MatrixException& me){
-	is_thrown =true;
-	ASSERT_STREQ("degenerate matrix", me.what());}
+  try {
+    Matrix<double> mt(12, 0);
+  } catch (const MatrixException &me) {
+    is_thrown = true;
+    ASSERT_STREQ("degenerate matrix", me.what());
+  }
 
-ASSERT_THAT(is_thrown, testing::Eq(true));
+  ASSERT_THAT(is_thrown, testing::Eq(true));
 };
 
-class TestMatrixClass: public testing::Test {
-public:	
+class TestMatrixClass : public testing::Test {
+public:
+  Matrix<int> GetMatrix_0() {
+    Matrix<int> m(2, 2);
+    m(0, 0) = 0;
+    m(0, 1) = 0;
+    m(1, 0) = 0;
+    m(1, 1) = 0;
+    return m;
+  };
+  Matrix<int> GetMatrix_1() {
+    Matrix<int> m(2, 2);
+    m(0, 0) = 1;
+    m(0, 1) = 2;
+    m(1, 0) = 3;
+    m(1, 1) = 4;
+    return m;
+  };
+  Matrix<int> GetMatrix_2() {
+    Matrix<int> m(2, 2);
+    m(0, 0) = 2;
+    m(0, 1) = 5;
+    m(1, 0) = 10;
+    m(1, 1) = 20;
+    return m;
+  };
+  Matrix<int> GetMatix2times3() {
+    Matrix<int> m(2, 3);
+    m(0, 0) = 1;
+    m(0, 1) = 2;
+    m(0, 2) = 3;
+    m(1, 0) = 4;
+    m(1, 1) = 5;
+    m(1, 2) = 6;
+    return m;
+  };
 
-Matrix<int> GetMatrix_0(){
-	Matrix<int> m(2,2);
-	m(0,0)=0; m(0,1)=0;
-	m(1,0)=0; m(1,1)=0;
-	return m;
-};
-Matrix<int> GetMatrix_1(){
-	Matrix<int> m(2,2);
-	m(0,0)=1; m(0,1)=2;
-	m(1,0)=3; m(1,1)=4;
-	return m;
-};
-Matrix<int> GetMatrix_2(){
-	Matrix<int> m(2,2);
-	m(0,0)=2; m(0,1)=5;
-	m(1,0)=10; m(1,1)=20;
-	return m;
-};
-Matrix<int> GetMatix2times3(){
-	Matrix<int> m(2,3);
-	m(0,0) = 1; m(0,1) = 2; m(0,2) = 3;	
-	m(1,0) = 4; m(1,1) = 5; m(1,2) = 6;
-	return m;
-};
-
-Matrix<int> GetMatix3times2(){
-	Matrix<int> m(3,2);
-	m(0,0) = 1; m(0,1) = 2;
-       	m(1,0) = 3; m(1,1) = 4; 
-	m(2,0) = 5; m(2,1) = 6;
-	return m;
-};
-};
-	
-TEST_F(TestMatrixClass, CreateAndInit2DMatrix){
-	Matrix<int> m = GetMatrix_1();  
-	ASSERT_THAT(m(0,0),testing::Eq(1)); 
-	ASSERT_THAT(m(0,1),testing::Eq(2));	
-	ASSERT_THAT(m(1,0),testing::Eq(3));
-	ASSERT_THAT(m(1,1),testing::Eq(4));
-};
-
-TEST_F(TestMatrixClass, AddEqOperator){
-	Matrix<int> m = GetMatrix_1();
-	Matrix<int> m2 = GetMatrix_1();
-	m+=m2;
-	ASSERT_THAT(m(0,0),testing::Eq(2));
-	ASSERT_THAT(m(0,1),testing::Eq(4));	
-	ASSERT_THAT(m(1,0),testing::Eq(6));
-	ASSERT_THAT(m(1,1),testing::Eq(8));
+  Matrix<int> GetMatix3times2() {
+    Matrix<int> m(3, 2);
+    m(0, 0) = 1;
+    m(0, 1) = 2;
+    m(1, 0) = 3;
+    m(1, 1) = 4;
+    m(2, 0) = 5;
+    m(2, 1) = 6;
+    return m;
+  };
 };
 
-TEST_F(TestMatrixClass, InitAddOperator){
-	Matrix<int> m1 = GetMatrix_1();
-	Matrix<int> m2 = GetMatrix_2();
-	Matrix<int> m = (m1 + m2) ;	
-	ASSERT_THAT(m(0,0),testing::Eq(3));
-	ASSERT_THAT(m(0,1),testing::Eq(7));	
-	ASSERT_THAT(m(1,0),testing::Eq(13));
-	ASSERT_THAT(m(1,1),testing::Eq(24));
+TEST_F(TestMatrixClass, CreateAndInit2DMatrix) {
+  Matrix<int> m = GetMatrix_1();
+  ASSERT_THAT(m(0, 0), testing::Eq(1));
+  ASSERT_THAT(m(0, 1), testing::Eq(2));
+  ASSERT_THAT(m(1, 0), testing::Eq(3));
+  ASSERT_THAT(m(1, 1), testing::Eq(4));
 };
 
-
-TEST_F(TestMatrixClass, MathAddOperator){
-	Matrix<int> m1 = GetMatrix_1();
-	Matrix<int> m2 = GetMatrix_2();
-	Matrix<int> m = (m1 + m2) ;	
-	m = m + m1; 
-	ASSERT_THAT(m(0,0),testing::Eq(4));
-	ASSERT_THAT(m(0,1),testing::Eq(9));	
-	ASSERT_THAT(m(1,0),testing::Eq(16));
-	ASSERT_THAT(m(1,1),testing::Eq(28));
+TEST_F(TestMatrixClass, AddEqOperator) {
+  Matrix<int> m = GetMatrix_1();
+  Matrix<int> m2 = GetMatrix_1();
+  m += m2;
+  ASSERT_THAT(m(0, 0), testing::Eq(2));
+  ASSERT_THAT(m(0, 1), testing::Eq(4));
+  ASSERT_THAT(m(1, 0), testing::Eq(6));
+  ASSERT_THAT(m(1, 1), testing::Eq(8));
 };
 
-TEST_F(TestMatrixClass, InitAddTwoDifferentDimMatricesExeption){
-bool is_thrown = false;
-Matrix<int> m0 = GetMatrix_0();
-Matrix<int> m1 = GetMatix2times3();
-	
-try{
-	m0+=m1;} 
-catch(const MatrixException& me ){
-	is_thrown = true;
-	ASSERT_STREQ( "different size matrices", me.what());}
+TEST_F(TestMatrixClass, InitAddOperator) {
+  Matrix<int> m1 = GetMatrix_1();
+  Matrix<int> m2 = GetMatrix_2();
+  Matrix<int> m = (m1 + m2);
+  ASSERT_THAT(m(0, 0), testing::Eq(3));
+  ASSERT_THAT(m(0, 1), testing::Eq(7));
+  ASSERT_THAT(m(1, 0), testing::Eq(13));
+  ASSERT_THAT(m(1, 1), testing::Eq(24));
 };
 
-TEST_F(TestMatrixClass, InitNumMultiplication){
-	Matrix<int> m = GetMatrix_1();
-	const int multiplier = 3;
-	
-	m*=multiplier;
-
-	ASSERT_THAT(m(0,0),testing::Eq(3));
-	ASSERT_THAT(m(0,1),testing::Eq(6));	
-	ASSERT_THAT(m(1,0),testing::Eq(9));
-	ASSERT_THAT(m(1,1),testing::Eq(12));
+TEST_F(TestMatrixClass, MathAddOperator) {
+  Matrix<int> m1 = GetMatrix_1();
+  Matrix<int> m2 = GetMatrix_2();
+  Matrix<int> m = (m1 + m2);
+  m = m + m1;
+  ASSERT_THAT(m(0, 0), testing::Eq(4));
+  ASSERT_THAT(m(0, 1), testing::Eq(9));
+  ASSERT_THAT(m(1, 0), testing::Eq(16));
+  ASSERT_THAT(m(1, 1), testing::Eq(28));
 };
 
+TEST_F(TestMatrixClass, InitAddTwoDifferentDimMatricesExeption) {
+  bool is_thrown = false;
+  Matrix<int> m0 = GetMatrix_0();
+  Matrix<int> m1 = GetMatix2times3();
 
-TEST_F(TestMatrixClass, LhsMultiplication){
-	Matrix<int> m1 = GetMatrix_1();
-	const int multiplier = 4;
-
-	Matrix<int> m = m1 * multiplier;
-
-	ASSERT_THAT(m(0,0),testing::Eq(4));
-	ASSERT_THAT(m(0,1),testing::Eq(8));	
-	ASSERT_THAT(m(1,0),testing::Eq(12));
-	ASSERT_THAT(m(1,1),testing::Eq(16));
+  try {
+    m0 += m1;
+  } catch (const MatrixException &me) {
+    is_thrown = true;
+    ASSERT_STREQ("different size matrices", me.what());
+  }
 };
 
+TEST_F(TestMatrixClass, InitNumMultiplication) {
+  Matrix<int> m = GetMatrix_1();
+  const int multiplier = 3;
 
-TEST_F(TestMatrixClass, RhsMultiplication){
-	Matrix<int> m1 = GetMatrix_1();
-	const int multiplier = 5;
+  m *= multiplier;
 
-	Matrix<int> m = multiplier * m1;
-
-	ASSERT_THAT(m(0,0),testing::Eq(5));
-	ASSERT_THAT(m(0,1),testing::Eq(10));	
-	ASSERT_THAT(m(1,0),testing::Eq(15));
-	ASSERT_THAT(m(1,1),testing::Eq(20));
+  ASSERT_THAT(m(0, 0), testing::Eq(3));
+  ASSERT_THAT(m(0, 1), testing::Eq(6));
+  ASSERT_THAT(m(1, 0), testing::Eq(9));
+  ASSERT_THAT(m(1, 1), testing::Eq(12));
 };
 
+TEST_F(TestMatrixClass, LhsMultiplication) {
+  Matrix<int> m1 = GetMatrix_1();
+  const int multiplier = 4;
 
-TEST_F(TestMatrixClass, LhsMultiplicationAndCopyAssignment){
-	Matrix<int> m1 = GetMatrix_1();
-	const int multiplier = 4;
-	Matrix<int> m; 
+  Matrix<int> m = m1 * multiplier;
 
-	m =  m1 * multiplier;
-
-	ASSERT_THAT(m(0,0),testing::Eq(4));
-	ASSERT_THAT(m(0,1),testing::Eq(8));	
-	ASSERT_THAT(m(1,0),testing::Eq(12));
-	ASSERT_THAT(m(1,1),testing::Eq(16));
+  ASSERT_THAT(m(0, 0), testing::Eq(4));
+  ASSERT_THAT(m(0, 1), testing::Eq(8));
+  ASSERT_THAT(m(1, 0), testing::Eq(12));
+  ASSERT_THAT(m(1, 1), testing::Eq(16));
 };
 
-TEST_F(TestMatrixClass, Transpose){
-	Matrix<int> m = GetMatix2times3();		
+TEST_F(TestMatrixClass, RhsMultiplication) {
+  Matrix<int> m1 = GetMatrix_1();
+  const int multiplier = 5;
 
-	m = m.getTranspose();
+  Matrix<int> m = multiplier * m1;
 
-	//m(0,0) = 1; m(0,1) = 2; m(0,2) = 3;	
-	//m(1,0) = 4; m(1,1) = 5; m(1,2) = 6;
-	//first row 
-	ASSERT_THAT(m(0,0),testing::Eq(1));
-	ASSERT_THAT(m(0,1),testing::Eq(4));
-	//second row
-	ASSERT_THAT(m(1,0),testing::Eq(2));
-	ASSERT_THAT(m(1,1),testing::Eq(5));
-	//last row
-	ASSERT_THAT(m(2,0),testing::Eq(3));
-	ASSERT_THAT(m(2,1),testing::Eq(6));
+  ASSERT_THAT(m(0, 0), testing::Eq(5));
+  ASSERT_THAT(m(0, 1), testing::Eq(10));
+  ASSERT_THAT(m(1, 0), testing::Eq(15));
+  ASSERT_THAT(m(1, 1), testing::Eq(20));
+};
+
+TEST_F(TestMatrixClass, LhsMultiplicationAndCopyAssignment) {
+  Matrix<int> m1 = GetMatrix_1();
+  const int multiplier = 4;
+  Matrix<int> m;
+
+  m = m1 * multiplier;
+
+  ASSERT_THAT(m(0, 0), testing::Eq(4));
+  ASSERT_THAT(m(0, 1), testing::Eq(8));
+  ASSERT_THAT(m(1, 0), testing::Eq(12));
+  ASSERT_THAT(m(1, 1), testing::Eq(16));
+};
+
+TEST_F(TestMatrixClass, Transpose) {
+  Matrix<int> m = GetMatix2times3();
+
+  m = m.getTranspose();
+
+  // m(0,0) = 1; m(0,1) = 2; m(0,2) = 3;
+  // m(1,0) = 4; m(1,1) = 5; m(1,2) = 6;
+  // first row
+  ASSERT_THAT(m(0, 0), testing::Eq(1));
+  ASSERT_THAT(m(0, 1), testing::Eq(4));
+  // second row
+  ASSERT_THAT(m(1, 0), testing::Eq(2));
+  ASSERT_THAT(m(1, 1), testing::Eq(5));
+  // last row
+  ASSERT_THAT(m(2, 0), testing::Eq(3));
+  ASSERT_THAT(m(2, 1), testing::Eq(6));
 }
 
+TEST_F(TestMatrixClass, MatrixSquare) {
+  Matrix<int> m1 = GetMatrix_1();
 
-TEST_F(TestMatrixClass, MatrixSquare){
-	Matrix<int> m1 = GetMatrix_1();
+  Matrix<int> m(m1 * m1);
 
-	Matrix<int> m(m1*m1);
-
-	ASSERT_THAT(m(0,0),testing::Eq(7));
-	ASSERT_THAT(m(0,1),testing::Eq(10));	
-	ASSERT_THAT(m(1,0),testing::Eq(15));
-	ASSERT_THAT(m(1,1),testing::Eq(22));
+  ASSERT_THAT(m(0, 0), testing::Eq(7));
+  ASSERT_THAT(m(0, 1), testing::Eq(10));
+  ASSERT_THAT(m(1, 0), testing::Eq(15));
+  ASSERT_THAT(m(1, 1), testing::Eq(22));
 };
 
-TEST_F(TestMatrixClass, Multiply3By2With2By3Matrices){
-	Matrix<int> m_n = GetMatix3times2();
-	Matrix<int> m_t(m_n.getTranspose());
-       	
-	Matrix<int> m(m_n*m_t);
+TEST_F(TestMatrixClass, Multiply3By2With2By3Matrices) {
+  Matrix<int> m_n = GetMatix3times2();
+  Matrix<int> m_t(m_n.getTranspose());
 
-//		m(0,0) = 1; m(0,1) = 2;
-//	       	m(1,0) = 3; m(1,1) = 4; 
-//		m(2,0) = 5; m(2,1) = 6;
-	//first row
-	ASSERT_THAT(m(0,0),testing::Eq(5));
-	ASSERT_THAT(m(0,1),testing::Eq(11));	
-	ASSERT_THAT(m(0,2),testing::Eq(17));	
-	//second row
-	ASSERT_THAT(m(1,0),testing::Eq(11));
-	ASSERT_THAT(m(1,1),testing::Eq(25));
-	ASSERT_THAT(m(1,2),testing::Eq(39));	
-	//third row
-	ASSERT_THAT(m(2,0),testing::Eq(17));
-	ASSERT_THAT(m(2,1),testing::Eq(39));
-	ASSERT_THAT(m(2,2),testing::Eq(61));
+  Matrix<int> m(m_n * m_t);
+
+  //		m(0,0) = 1; m(0,1) = 2;
+  //	       	m(1,0) = 3; m(1,1) = 4;
+  //		m(2,0) = 5; m(2,1) = 6;
+  // first row
+  ASSERT_THAT(m(0, 0), testing::Eq(5));
+  ASSERT_THAT(m(0, 1), testing::Eq(11));
+  ASSERT_THAT(m(0, 2), testing::Eq(17));
+  // second row
+  ASSERT_THAT(m(1, 0), testing::Eq(11));
+  ASSERT_THAT(m(1, 1), testing::Eq(25));
+  ASSERT_THAT(m(1, 2), testing::Eq(39));
+  // third row
+  ASSERT_THAT(m(2, 0), testing::Eq(17));
+  ASSERT_THAT(m(2, 1), testing::Eq(39));
+  ASSERT_THAT(m(2, 2), testing::Eq(61));
 };
-TEST_F(TestMatrixClass, MultiplyNew3By2With2By3Matrices){
-	Matrix<int> m_n = GetMatix3times2();
-	Matrix<int> m_t(m_n.getTranspose());
-       	
-	Matrix<int> m(m_n&m_t);
-//		m(0,0) = 1; m(0,1) = 2;
-//	       	m(1,0) = 3; m(1,1) = 4; 
-//		m(2,0) = 5; m(2,1) = 6;
+TEST_F(TestMatrixClass, MultiplyNew3By2With2By3Matrices) {
+  Matrix<int> m_n = GetMatix3times2();
+  Matrix<int> m_t(m_n.getTranspose());
 
-	//first row
-	ASSERT_THAT(m(0,0),testing::Eq(5));
-	ASSERT_THAT(m(0,1),testing::Eq(11));	
-	ASSERT_THAT(m(0,2),testing::Eq(17));	
-	//second row
-	ASSERT_THAT(m(1,0),testing::Eq(11));
-	ASSERT_THAT(m(1,1),testing::Eq(25));
-	ASSERT_THAT(m(1,2),testing::Eq(39));	
-	//third row
-	ASSERT_THAT(m(2,0),testing::Eq(17));
-	ASSERT_THAT(m(2,1),testing::Eq(39));
-	ASSERT_THAT(m(2,2),testing::Eq(61));
-};	
+  Matrix<int> m(m_n & m_t);
+  //		m(0,0) = 1; m(0,1) = 2;
+  //	       	m(1,0) = 3; m(1,1) = 4;
+  //		m(2,0) = 5; m(2,1) = 6;
 
-TEST_F(TestMatrixClass, RhsMultiplicationAndCopyAssignment){
-	Matrix<int> m1 = GetMatrix_1();
-	const int multiplier = 5;
-	Matrix<int> m; 
-	
-	m = multiplier * m1;
-
-	ASSERT_THAT(m(0,0),testing::Eq(5));
-	ASSERT_THAT(m(0,1),testing::Eq(10));	
-	ASSERT_THAT(m(1,0),testing::Eq(15));
-	ASSERT_THAT(m(1,1),testing::Eq(20));
+  // first row
+  ASSERT_THAT(m(0, 0), testing::Eq(5));
+  ASSERT_THAT(m(0, 1), testing::Eq(11));
+  ASSERT_THAT(m(0, 2), testing::Eq(17));
+  // second row
+  ASSERT_THAT(m(1, 0), testing::Eq(11));
+  ASSERT_THAT(m(1, 1), testing::Eq(25));
+  ASSERT_THAT(m(1, 2), testing::Eq(39));
+  // third row
+  ASSERT_THAT(m(2, 0), testing::Eq(17));
+  ASSERT_THAT(m(2, 1), testing::Eq(39));
+  ASSERT_THAT(m(2, 2), testing::Eq(61));
 };
 
-TEST_F(TestMatrixClass, VertorSum){
-	std::vector<int> vec_1{1,2,3}, vec_2{7,8,9};
-	Matrix<int> m_1(vec_1), m_2(vec_2), m_sum;
-	
-	m_sum = m_1 + m_2;
-	
-	ASSERT_THAT(m_sum(0,0),testing::Eq(8));
-	ASSERT_THAT(m_sum(0,1),testing::Eq(10));	
-	ASSERT_THAT(m_sum(0,2),testing::Eq(12));
+TEST_F(TestMatrixClass, RhsMultiplicationAndCopyAssignment) {
+  Matrix<int> m1 = GetMatrix_1();
+  const int multiplier = 5;
+  Matrix<int> m;
+
+  m = multiplier * m1;
+
+  ASSERT_THAT(m(0, 0), testing::Eq(5));
+  ASSERT_THAT(m(0, 1), testing::Eq(10));
+  ASSERT_THAT(m(1, 0), testing::Eq(15));
+  ASSERT_THAT(m(1, 1), testing::Eq(20));
 };
 
-TEST_F(TestMatrixClass, SpeedNaiveMulti){
-	size_t rowN = 200;
-        size_t colN = 200;
-	Matrix<int> m(rowN,colN);
-	for(size_t i=0;i < rowN; i++)
-		for(size_t j=0; j<colN; j++)
-			m(i,j) = i+j;
+TEST_F(TestMatrixClass, VertorSum) {
+  std::vector<int> vec_1{1, 2, 3}, vec_2{7, 8, 9};
+  Matrix<int> m_1(vec_1), m_2(vec_2), m_sum;
 
-	Matrix<int> m_multi(m*m);	
+  m_sum = m_1 + m_2;
+
+  ASSERT_THAT(m_sum(0, 0), testing::Eq(8));
+  ASSERT_THAT(m_sum(0, 1), testing::Eq(10));
+  ASSERT_THAT(m_sum(0, 2), testing::Eq(12));
 };
 
-TEST_F(TestMatrixClass, SpeedWithTransformMulti){
-	size_t rowN = 200;
-        size_t colN = 200;
-	Matrix<int> m(rowN,colN);
-	for(size_t i=0;i < rowN; i++)
-		for(size_t j=0; j<colN; j++)
-			m(i,j) = i+j;
+TEST_F(TestMatrixClass, SpeedNaiveMulti) {
+  size_t rowN = 200;
+  size_t colN = 200;
+  Matrix<int> m(rowN, colN);
+  for (size_t i = 0; i < rowN; i++)
+    for (size_t j = 0; j < colN; j++)
+      m(i, j) = i + j;
 
-	Matrix<int> m_multi(m*m);	
+  Matrix<int> m_multi(m * m);
 };
+
+TEST_F(TestMatrixClass, SpeedWithTransformMulti) {
+  size_t rowN = 200;
+  size_t colN = 200;
+  Matrix<int> m(rowN, colN);
+  for (size_t i = 0; i < rowN; i++)
+    for (size_t j = 0; j < colN; j++)
+      m(i, j) = i + j;
+
+  Matrix<int> m_multi(m * m);
+};
+
